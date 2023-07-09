62301df67262ea12240110cca6013adb21e3c530
Everson Araujo <araujoeverson@icloud.com>
2023-07-09 11:34:19 -0300

    linhas

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
-const form = document.getElementById('form-atividade');
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
+const form = document.getElementById("form-atividade");
+let linhas ='';
 
-    const inputNomeAtividade = document.getElementById('nome-atividade');
-    const inputNotaAtividade = document.getElementById('nota-atividade');
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    alert (`Atividade: ${inputNomeAtividade.value} - Nota: ${inputNotaAtividade.value}`);
+  const inputNomeAtividade = document.getElementById("nome-atividade");
+  const inputNotaAtividade = document.getElementById("nota-atividade");
+
+  let linha = '<tr>';
+  linha += `<td>${inputNomeAtividade.value}</td>`;
+  linha += `<td>${inputNotaAtividade.value}</td>`;
+  linha += `<td>${inputNotaAtividade.value >= 7 ? "Aprovado" : "Reprovado"}</td>`;
+  linha += "</tr>";
+
+  linhas += linha;
+
+  const corpoTabela = document.querySelector("tbody");
+  corpoTabela.innerHTML += linha;
 });
